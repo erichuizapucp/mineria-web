eb8c593ead8221566358b2b2c0a2f598e1e99866
maria <maria.delaguardav@gmail.com>
2025-08-21 21:55:27 -0500

    2 new attributes

--- sesion-de-clase-01/amazon_scraper/parser.py
+++ sesion-de-clase-01/amazon_scraper/parser.py
@@ -4,10 +4,14 @@ def parse_product(soup: BeautifulSoup):
     product = {}
 
     # Establecer los selectores del DOM y el CSS
-    title_tag = soup.select_one("span#productTitle")
-    price_tag = soup.select_one("span.a-price-whole")
+    title_tag = soup.select_one("span#productTitle") # Busca id
+    price_tag = soup.select_one("span.a-price-whole") # Busca clase
+    marca_tag = soup.select_one("span.a-size-base.po-break-word")
+    delivery_tag = soup.select_one("span.a-size-base.a-color-secondary")
 
     product["title"] = title_tag.get_text(strip=True) if title_tag else None
     product["price"] = price_tag.get_text(strip=True) if price_tag else None
+    product["marca"] = marca_tag.get_text(strip=True) if marca_tag else None
+    product["delivery"] = delivery_tag.get_text(strip=True) if delivery_tag else None
 
     return product
